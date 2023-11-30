ad443f55c673e7f7f374fd6227de427fa8dd08be
hieupham250 <147390672+hieupham250@users.noreply.github.com>
2023-11-30 13:26:13 +0700

    bt3.c

--- bt3.c
+++ bt3.c
@@ -12,11 +12,11 @@ int lenghtChar(char *str){
 	return count;
 }
 //so sanh
-int compare(int a, int b){
+void compareString(int a, int b){
 	if(a>b){
-		printf("chuoi thu 1 dai hon");
+		printf("chuoi thu 1 dai hon chuoi thu 2");
 	}else if(a<b){
-		printf("chuoi thu 2 dai hon");
+		printf("chuoi thu 2 dai hon chuoi thu 1");
 	}else{
 		printf("chuoi 1 va chuoi 2 bang nhau");
 	}
@@ -33,6 +33,6 @@ int main(){
 	printf("Do dai cua chuoi 1: %d\n",lenght1);
 	int lenght2 = lenghtChar(str2);
 	printf("Do dai cua chuoi 2: %d\n",lenght2);
-	int stringCompare = compare(lenght1,lenght2);
+	compareString(lenght1,lenght2);
 	return 0;
 }
